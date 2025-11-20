44a47dd84d3395f32c2c31c46590f5094befb4a6
Lauren Herregodts <lauren.herregodts@cellcarta.com>
2025-11-20 09:18:36 -0500

    Add models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -2,14 +2,29 @@ from django.contrib import admin
 from .models import CarMake, CarModel
 
 
-# Register your models here.
-admin.site.register(CarMake)
-admin.site.register(CarModel)
-# CarModelInline class
+# Inline: show CarModel entries inside CarMake admin page
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1  # how many empty forms to display
+    fields = ('name', 'type', 'year')  # matches your model fields
+    show_change_link = True  # adds a link to edit each model
 
-# CarModelAdmin class
 
-# CarMakeAdmin class with CarModelInline
+# Custom admin for CarModel
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year')
+    list_filter = ('type', 'year', 'car_make')
+    search_fields = ('name',)
+    ordering = ('car_make', 'name')
 
-# Register models here
 
+# Custom admin for CarMake with inline CarModels
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+    inlines = [CarModelInline]
+
+
+# Register models
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
