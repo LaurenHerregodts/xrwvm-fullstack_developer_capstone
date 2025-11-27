c0a292c085880a47c178c63ef312c708efec4d3a
Lauren Herregodts <111417120+LaurenHerregodts@users.noreply.github.com>
2025-11-27 15:53:45 +0100

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -17,7 +17,7 @@ def get_request(endpoint, **kwargs):
     params = ""
     if (kwargs):
         for key, value in kwargs.items():
-            params=params + key + "= " + value + "&"
+            params= params + key + "= " + value + "&"
 
     request_url = backend_url + endpoint + "?" + params
 
@@ -28,6 +28,7 @@ def get_request(endpoint, **kwargs):
         return response.json()
     except:
         # If any error occurs
+        print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
 
